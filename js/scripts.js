7bdc719a9c112de5860634d45586620a3d5c5ab1
Eniola Oseni <eniola.oseni@verraki.com>
2022-04-19 20:58:28 +0100

    Fixed Flexslider

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 // Can also be used with $(document).ready()
 $(window).load(function() {
     $('.flexslider').flexslider({
-      animation: "slide",
       controlsContainer: $(".custom-controls-container"),
       customDirectionNav: $(".custom-navigation a"),
       controlNav: true,
   
     });
     $('.flexslider2').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container2"),
         customDirectionNav: $(".custom-navigation2 a"),
         controlNav: true,
@@ -16,35 +14,30 @@ $(window).load(function() {
    
       });
       $('.flexslider3').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container3"),
         customDirectionNav: $(".custom-navigation3 a"),
         controlNav: true,
         directionNav: true,
       });
       $('.flexslider4').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container4"),
         customDirectionNav: $(".custom-navigation4 a"),
         controlNav: true,
         directionNav: true,
       });
       $('.flexslider5').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container5"),
         customDirectionNav: $(".custom-navigation5 a"),
         controlNav: true,
         directionNav: true,
       });
       $('.flexslider6').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container6"),
         customDirectionNav: $(".custom-navigation6 a"),
         controlNav: true,
         directionNav: true,
       });
       $('.flexslider7').flexslider({
-        animation: "slide",
         controlsContainer: $(".custom-controls-container7"),
         customDirectionNav: $(".custom-navigation7 a"),
         controlNav: true,
